85e563b92577ae91acf7680e365d24c1d0e489d2
Сергей Мдинарадзе <s@mdinaradze.ru>
2022-08-08 22:12:00 +0300

    delete comment

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -56,17 +56,6 @@ class CommentsViewSet(viewsets.ModelViewSet):
         serializer.save(author=self.request.user)
 
 
-# class FollowViewSet(viewsets.ModelViewSet):
-#     serializer_class = FollowSerializer
-#     filter_backends = (filters.SearchFilter,)
-#     search_fields = ('following__username',)
-
-#     def get_queryset(self):
-#         return self.request.user.follower.all()
-
-#     def perform_create(self, serializer):
-#         serializer.save(user=self.request.user)
-
 class FollowViewSet(mixins.ListModelMixin,
                     mixins.CreateModelMixin,
                     viewsets.GenericViewSet):
